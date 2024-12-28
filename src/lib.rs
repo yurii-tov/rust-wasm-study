8a6503729858629b8b24db39f61bd9fdb5325c15
Yurii <juretz32@yandex.ru>
2024-12-28 20:08:48 +0300

    Canvas version

--- src/lib.rs
+++ src/lib.rs
@@ -112,4 +112,16 @@ impl Universe {
     pub fn render(&self) -> String {
         self.to_string()
     }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
 }
