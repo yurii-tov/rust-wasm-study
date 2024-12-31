8b5f2a9057494142604383d0a4515d14badf143f
Yurii <juretz32@yandex.ru>
2024-12-31 12:38:48 +0300

    Code cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -55,6 +55,20 @@ impl Universe {
         }
         count
     }
+
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
 }
 
 #[wasm_bindgen]
@@ -135,6 +149,7 @@ impl Universe {
         self.cells.as_ptr()
     }
 
+    /// Get number of living cells
     pub fn living(&self) -> u32 {
         self.cells.iter().filter(|x| **x == Cell::Alive).count() as u32
     }
@@ -147,16 +162,7 @@ impl Universe {
         let idx = self.get_index(row, column);
         self.cells[idx].toggle();
     }
-}
 
-impl Default for Universe {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
-#[wasm_bindgen]
-impl Universe {
     /// Set the width of the universe.
     ///
     /// Resets all cells to the dead state.
@@ -171,21 +177,5 @@ impl Universe {
     pub fn set_height(&mut self, height: u32) {
         self.height = height;
         self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
-    }
-}
-
-impl Universe {
-    /// Get the dead and alive values of the entire universe.
-    pub fn get_cells(&self) -> &[Cell] {
-        &self.cells
-    }
-
-    /// Set cells to be alive in a universe by passing the row and column
-    /// of each cell as an array.
-    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
-        for (row, col) in cells.iter().cloned() {
-            let idx = self.get_index(row, col);
-            self.cells[idx] = Cell::Alive;
-        }
     }
 }
