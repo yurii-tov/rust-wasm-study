c218a93fb3b58cc74e3c3816610f478b762677ac
Yurii <juretz32@yandex.ru>
2025-01-01 18:01:39 +0300

    Add profiling code

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use web_sys::console;
+
 pub fn set_panic_hook() {
     // When the `console_error_panic_hook` feature is enabled, we can call the
     // `set_panic_hook` function at least once during initialization, and then
@@ -14,5 +16,22 @@ pub fn set_panic_hook() {
 macro_rules! log {
     ( $( $t:tt )* ) => {
         web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
     }
 }
