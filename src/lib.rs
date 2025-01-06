f8bd57c0a2366ad5d800b5fa005f91e26c3ccb7c
Yurii <juretz32@yandex.ru>
2025-01-06 21:07:08 +0300

    Simplify rule set

--- src/lib.rs
+++ src/lib.rs
@@ -140,20 +140,9 @@ impl Universe {
                 let cell = self.cells[idx];
                 let live_neighbors = self.live_neighbor_count(row, col);
                 let next_cell = match (cell, live_neighbors) {
-                    // Rule 1: Any live cell with fewer than two live neighbours
-                    // dies, as if caused by underpopulation.
-                    (Cell::Alive, x) if x < 2 => Cell::Dead,
-                    // Rule 2: Any live cell with two or three live neighbours
-                    // lives on to the next generation.
-                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
-                    // Rule 3: Any live cell with more than three live
-                    // neighbours dies, as if by overpopulation.
-                    (Cell::Alive, x) if x > 3 => Cell::Dead,
-                    // Rule 4: Any dead cell with exactly three live neighbours
-                    // becomes a live cell, as if by reproduction.
-                    (Cell::Dead, 3) => Cell::Alive,
-                    // All other cells remain in the same state.
-                    (otherwise, _) => otherwise,
+                    (Cell::Alive, x) if x < 2 || x > 3 => Cell::Dead,
+                    (Cell::Alive, 2) | (_, 3) => Cell::Alive,
+                    (x, _) => x,
                 };
 
                 match (cell, next_cell) {
